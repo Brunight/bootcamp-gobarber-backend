f24718e1c356e331c6b908a2a3914d0f25b7ce8b
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2020-02-12 15:43:14 -0300

    feat: Mailing support

---
+++ src/config/mail.js
@@ -0,0 +1,22 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7119e547483a0b',
+    pass: '47ef0c776a37ce',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Email providers examples:
+ * Amazon SES <-
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * GMail (not recommended)
+ * Mailtrap (DEV) <-
+ */
